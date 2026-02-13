c3b524c25fa3b34b3a70d460d456dad336854846
Om Utkarsh <omutkarsh2005@gmail.com>
2026-02-13 19:47:21 +0530

    My first commit on this repo!

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
+    
+    name = request.GET.get("name")
+    if name is not None :
+        return JsonResponse({"message":f"Namaste, {name}"})
     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
 
 urlpatterns = [
